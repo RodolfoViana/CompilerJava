cd38c4674746d10dea05cef9b856ac2d8b68be0f
Isabelle Cardoso Ferreira <bellezeta@gmail.com>
2015-07-11 19:54:32 -0300

    Adicionei um .rar de um exemplo que me baseei na net. [2]

--- src/jflexcup/SemanticAnalyser.java
+++ src/jflexcup/SemanticAnalyser.java
@@ -18,8 +18,7 @@ public class SemanticAnalyser {
 	}
 	
 	public static void addID(String id){
-<<<<<<< HEAD
-		
+
 		if(ids.contains(id)){
 			System.err.println("Variável "+id+" já existe.");
 			//System.exit(1);
@@ -37,39 +36,6 @@ public class SemanticAnalyser {
 		}else{
 			ids.add(id);
 			System.out.println("Adicionei a id: "+id+" de tipo "+type+".");
-		}		
-=======
-
-		if(ids.contains(id)){
-			System.err.println("Variável com esse nome '" + id + "' já existe.");
-			System.exit(1);
-		}else{
-			ids.add(id);
-			System.out.println("Adicionei a id: "+id);
-		}	
-	}
-	
-	/**
-	 * funcao para adicionar uma variavel
-	 * 
-	 * @param nome
-	 * @param tipo
-	 */
-	public void addVariavel(String nome, String tipo) {
-		if (!tipos.contains(tipo.toLowerCase())) {
-			System.out.println("Java não aceita variaveis com o tipo: "
-					+ tipo);
-			System.exit(1);
-		} else {
-			if (variaveis.containsKey(nome) || ids.contains(nome)) {
-				System.out.println("Variável com esse nome '" + nome + "' já existe.");
-				System.exit(1);
-			} else {
-				ids.add(nome);
-				variaveis.put(nome, tipo.toLowerCase());
-			}
 		}
->>>>>>> a0b4e29c95bbf69362bc17c9edfebccbc9ebf07a
 	}
-
 }
